8d5244a040081ac6a43d64cfd75aab88418f59f2
Maj Knez <actuallydoc@gmail.com>
2023-02-21 16:39:43 +0100

    Init commit
    Testing my ESP32 web app to turn on lights

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/disable',
+        destination: 'http://192.168.1.8/disable',
+      },
+      {
+        source: '/enable',
+        destination: 'http://192.168.1.8/enable',
+      },
+      {
+        source: '/status',
+        destination: 'http://192.168.1.8/status',
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
